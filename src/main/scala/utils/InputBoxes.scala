8aa6a14bc2dd9d0ed3ab7c66ea36cf098b06056c
mgpai <64056966+mgpai22@users.noreply.github.com>
2023-07-15 00:48:31 -0400

    use Seq instead of Lists and use Long instead of Double

--- src/main/scala/utils/InputBoxes.scala
+++ src/main/scala/utils/InputBoxes.scala
@@ -1,66 +1,45 @@
 package utils
 
 import org.ergoplatform.appkit._
+import org.ergoplatform.sdk.ErgoToken
 
 import java.util
-import scala.collection.mutable.ListBuffer
+import scala.collection.JavaConverters._
 
 class InputBoxes(val ctx: BlockchainContext) {
 
-  def getBoxById(id: String): Array[InputBox] ={
+  def getBoxById(id: String): Array[InputBox] = {
     this.ctx.getBoxesById(id)
   }
 
-  def getInputs(amountList: List[Double], senderAddress: Address, tokenListParam: List[List[String]] = null, amountTokens: List[List[Long]] = null): util.List[InputBox] ={
-    val amountTotal: Long = (Parameters.OneErg * amountList.sum).toLong
-    var tokenList = new ListBuffer[ErgoToken]()
-    var tokenList1 = new ListBuffer[ListBuffer[ErgoToken]]()
-    var tList = new ListBuffer[ErgoToken]()
-    var tokenList2 = new util.ArrayList[ErgoToken]()
-    var tokenList3 = new util.ArrayList[ErgoToken]()
-    var tokenAmountCounter = 0
-    if(tokenListParam !=null && amountTokens == null){
-      for(token <- tokenListParam){
-        for(x <- token){
-          val t: ErgoToken =  new ErgoToken(x, 1)
-          tokenList.append(t)
-        }
-      }
+  def getInputs(
+      amountList: Seq[Long],
+      senderAddress: Address,
+      tokenListParam: Option[Seq[Seq[String]]] = None,
+      amountTokens: Option[Seq[Seq[Long]]] = None
+  ): Seq[InputBox] = {
+    val amountTotal: Long = amountList.sum
+    val tokens = (tokenListParam, amountTokens) match {
+      case (Some(tlp), Some(at)) =>
+        tlp
+          .zip(at)
+          .flatMap { case (token, amounts) =>
+            token.zip(amounts).map { case (t, a) => new ErgoToken(t, a) }
+          }
+          .asJava
+      case (Some(tlp), None) =>
+        tlp.flatten.map(new ErgoToken(_, 1)).asJava
+      case _ =>
+        new util.ArrayList[ErgoToken]()
     }
-    else if(tokenListParam !=null && amountTokens != null){
-      for(token <- tokenListParam){
-        var tokenAmountCounterLocal = 0
-        var tokenAmountList = amountTokens.apply(tokenAmountCounter)
-        for(x <- token){
-          var tokenAmm = tokenAmountList.apply(tokenAmountCounterLocal)
-          tList.append(new ErgoToken(x, tokenAmm))
-          tokenAmountCounterLocal = tokenAmountCounterLocal + 1
-        }
-        tokenAmountCounter = tokenAmountCounter + 1
-        tokenList1.append(tList)
-        tList = new ListBuffer[ErgoToken]()
-      }
-      var res: ListBuffer[ErgoToken] =  new ListBuffer[ErgoToken]()
-      for(i <- tokenList1){
-        res = res ++ i
-      }
-      for (i <- res) {
-        tokenList2.add(i)
-      }
-    }
-    if(tokenListParam !=null && amountTokens == null){
-      for (i <- tokenList) {
-        tokenList3.add(i)
-      }
-      return BoxOperations.createForSender(senderAddress, this.ctx).withAmountToSpend(amountTotal)
-        .withTokensToSpend(tokenList3).withInputBoxesLoader(new ExplorerAndPoolUnspentBoxesLoader()).loadTop()
-    }
-    if(tokenListParam == null){
-      return BoxOperations.createForSender(senderAddress, this.ctx).withAmountToSpend(amountTotal)
-        .withInputBoxesLoader(new ExplorerAndPoolUnspentBoxesLoader()).loadTop()
-    }
-    return BoxOperations.createForSender(senderAddress, this.ctx).withAmountToSpend(amountTotal)
-      .withTokensToSpend(tokenList2).withInputBoxesLoader(new ExplorerAndPoolUnspentBoxesLoader()).loadTop()
+
+    BoxOperations
+      .createForSender(senderAddress, this.ctx)
+      .withAmountToSpend(amountTotal)
+      .withTokensToSpend(tokens)
+      .withInputBoxesLoader(new ExplorerAndPoolUnspentBoxesLoader())
+      .loadTop()
+      .asScala
   }
 
 }
