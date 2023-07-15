e6635bda341593084ef0eb1593976f673cd49fe9
mgpai <64056966+mgpai22@users.noreply.github.com>
2023-07-15 00:47:38 -0400

    add dummy contract

--- src/main/scala/utils/ContractCompile.scala
+++ src/main/scala/utils/ContractCompile.scala
@@ -4,6 +4,16 @@ import org.ergoplatform.appkit._
 import scorex.crypto.hash
 
 class ContractCompile(ctx: BlockchainContext) {
+
+  def compileDummyContract(
+      contract: String = "sigmaProp(true)"
+  ): ErgoContract = {
+    this.ctx.compileContract(
+      ConstantsBuilder.empty(),
+      contract
+    )
+  }
+
   def compileProxyContract(
       contract: String,
       minerFee: Long
